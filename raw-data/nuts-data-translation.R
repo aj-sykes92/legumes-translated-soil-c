4af87cf27ba651d21f8d227ad5f29b8ab40bef26
Alasdair Sykes <alasdair.sykes@gmail.com>
2020-12-21 21:44:37 +0000

    Sourced Eurostat NUTS level 2 data and translated to rds

---
+++ raw-data/nuts-data-translation.R
@@ -0,0 +1,11 @@
+library(sp)
+
+# read in Lvl. 2 NUTS regions in WGS84 projection and check
+nuts_fname <- find_onedrive("GIS data repository", "ref-nuts-2016-20m/NUTS_RG_20M_2016_4326_LEVL_2.shp/NUTS_RG_20M_2016_4326_LEVL_2.shp")
+Shp_nuts <- shapefile(nuts_fname)
+plot(Shp_nuts)
+Shp_nuts@proj4string
+unique(Shp_nuts@data$NUTS_ID)
+
+# write to 
+write_rds(Shp_nuts, "raw-data/nuts-shapefiles/nuts-lvl2-wgs84.rds")
